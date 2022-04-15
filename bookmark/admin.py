ef071347bd9d78b763a56f1f813426e440bb1baa
OSHMOS <ojames9707@gmail.com>
2022-04-15 13:54:40 +0900

    북마크 테이블 정의하여 admin 사이트에 반영 후 데이터베이스 변경 사항 반영하기

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Bookmark
 
 # Register your models here.
+class BookmarkAdmiin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url')
+
+admin.site.register(Bookmark)
