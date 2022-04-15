42fb637dbce1477998ce84352f05d7471c7994a8
OSHMOS <ojames9707@gmail.com>
2022-04-15 14:19:53 +0900

    북마크 애플리케이션 개발 코딩하기 URLconf

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bookmark.views import BookmarkLV, BookmarkDV
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bookmark/', BookmarkLV_as.view(), name='index'),
+    path('bookmark/<int:pk>/', BookmarkDV_as.view(), name='detail'),
 ]
